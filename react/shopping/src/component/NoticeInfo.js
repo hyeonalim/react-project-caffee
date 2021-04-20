ef6e9ab6996107e4c91bb59386a46fc539043d09
hyeonalim <82284927+hyeonalim@users.noreply.github.com>
2021-04-21 00:20:08 +0900

    Update NoticeInfo.js

--- react/shopping/src/component/NoticeInfo.js
+++ react/shopping/src/component/NoticeInfo.js
@@ -1,31 +1,17 @@
-import useFetch from "../hooks/useFetch";
-import { useParams } from "react-router-dom";
+import { useState } from "react"
 
-export default function NoticeInfo() {
-    const { id } = useParams();
+export default function Noticeinfo({ notice: n }) {
+    const [notice, setNotice] = useState(n);
+    const [isShow, setIsShow] = useState(false);
 
-    const notices = useFetch(`http://localhost:3001/notices?id=${id}`);
-    const infos = useFetch(`http://localhost:3001/notices_info?id=${id}`);
+    function toggleShow() {
+        setIsShow(!isShow);
+    }
 
     return (
-        <>
-            <h2>Notice {id}</h2>
-            <table>
-                <tbody>
-
-                    {notices.map(notice => (
-                        <li key={notice.id}>
-                            <h1>{notice.name}</h1>
-                        </li>
-                    ))}
-
-                    {infos.map(info => (
-                        <li key={info.id}>
-                            <p>{info.info}</p>
-                        </li>
-                    ))}
-                </tbody>
-            </table>
-        </>
+        <div className="list_notice">
+            <li><button onClick={toggleShow}>{notice.name}</button></li>
+            {isShow && notice.info}
+        </div>
     );
 }
