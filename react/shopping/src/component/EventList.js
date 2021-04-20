3a5a06671ff7827cb896a735bf66329c2db0f105
hyeonalim <82284927+hyeonalim@users.noreply.github.com>
2021-04-21 00:22:37 +0900

    Update EventList.js

--- react/shopping/src/component/EventList.js
+++ react/shopping/src/component/EventList.js
@@ -1,19 +1,18 @@
 import useFetch from "../hooks/useFetch";
 import { Link } from "react-router-dom";
 
-export default function EventList() {
-    const events = useFetch('http://localhost:3001/events');
+export default function ItemList() {
+    const items = useFetch('http://localhost:3001/items');
 
     return (
       <>
-        <ul className="list_event">
-        {events.map(event => (
-          <li key={event.id}>
-            <Link to={`/event/${event.id}`}>
-              <img src={`${event.img}`}></img>
-            </Link>
-                {event.start} ~ {event.end}
-                <p>{event.ing}</p>
+        <ul className="list_day">
+        {items.map(item => (
+          <li key={item.id}>
+            <Link to={`/item/${item.no}`}>
+              <img src={`${item.img}`}></img>
+              {item.name}
+              </Link>
           </li>
         ))}
       </ul>
