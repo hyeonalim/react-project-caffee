861bdcf7079cd7e85c0353597161dee5e429311b
Limhyeona <82284927+hyeonalim@users.noreply.github.com>
2021-04-28 23:37:13 +0900

    Update Item.js

--- react/shopping/src/component/Item.js
+++ react/shopping/src/component/Item.js
@@ -3,17 +3,20 @@ import {useParams} from "react-router-dom";
 import Info from "./Info";
 
 export default function Item(){
+    //각 아이템의 번호 가져오기
     const {no}= useParams();
 
-    const infos = useFetch(`http://localhost:3001/infos?no=${no}`);
+    //각 아이템의 정보 가져오기
+    const items = useFetch(`http://localhost:3001/items?no=${no}`);
 
+    //아이템 정보 나열하기
     return( 
     <>
     <h2>Item {no}</h2>
     <table>
         <tbody>
-            {infos.map(info => (
-                <Info info={info} key={info.id}/>
+            {items.map(item => (
+                <Info item={item} key={item.id}/>
             ))}
         </tbody>
     </table>
